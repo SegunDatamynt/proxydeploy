91056fd55944f9288be01fec853c6d4af1234de8
Prosperibe12 <prosperibe12@gmail.com>
2023-01-11 22:23:21 +0100

    added db settings

--- project_proxydeploy/settings.py
+++ project_proxydeploy/settings.py
@@ -67,7 +67,23 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    # 'postgress': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': config('NAME'), 
+    #     'USER': config('USER'), 
+    #     'PASSWORD': config('PASSWORD'),
+    #     'HOST':  config('HOST'), 
+    #     'PORT': config('PORT'),
+    # },
+    # 'mysql': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': config('NAME'), 
+    #     'USER': config('USER'), 
+    #     'PASSWORD': config('PASSWORD'),
+    #     'HOST':  config('HOST'), 
+    #     'PORT': config('PORT'),
+    # }
 }
 
 # Password validation
